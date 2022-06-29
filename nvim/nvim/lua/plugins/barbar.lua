e80fcca3507ea1c4db9eab159407d9577d19b655
Ratheesh S <ratheeshreddy@gmail.com>
2022-06-29 18:53:44 +0530

    barbar: Adjust correct commands

--- nvim/nvim/lua/plugins/barbar.lua
+++ nvim/nvim/lua/plugins/barbar.lua
@@ -2,49 +2,49 @@
 -- Ratheesh
 -- Copyright(c) 2022
 --
-require'bufferline'.setup {
+require'bufferline'.setup ({
 	animation               = true,
 	auto_hide               = false,
 	tabpages                = true,
 	closable                = true,
 	clickable               = true,
 	-- exclude_ft              = {'javascript'},
-  exclude_name            = {'package.json', "COMMIT_EDITMSG"},
-  icons                   = 'both',
-  icon_custom_colors      = false,
-  icon_separator_active   = '▌',
-  icon_separator_inactive = '',
-  icon_close_tab          = '',
-  icon_close_tab_modified = '●',
-  icon_pinned             = '車',
-  insert_at_end           = false,
-  insert_at_start         = false,
-  maximum_padding         = 1,
-  maximum_length          = 20,
-  semantic_letters        = true,
-  letters                 = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
-  no_name_title           = nil,
-}
+	exclude_name            = {'package.json', "COMMIT_EDITMSG"},
+	icons                   = 'both',
+	icon_custom_colors      = false,
+	icon_separator_active   = '▌',
+	icon_separator_inactive = '',
+	icon_close_tab          = '',
+	icon_close_tab_modified = '●',
+	icon_pinned             = '車',
+	insert_at_end           = false,
+	insert_at_start         = false,
+	maximum_padding         = 1,
+	maximum_length          = 20,
+	semantic_letters        = true,
+	letters                 = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+	no_name_title           = nil,
+})
 
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map('n', '<Leader>h', '<Cmd>BufferLinePrevious<CR>', opts)
-map('n', '<Leader>l', '<Cmd>BufferLineNext<CR>',     opts)
-map('n', '<Leader>1', '<Cmd>BufferLineGotoBuffer 3<CR>',   opts)
-map('n', '<Leader>2', '<Cmd>BufferLineGotoBuffer 3<CR>',   opts)
-map('n', '<Leader>3', '<Cmd>BufferLineGotoBuffer 3<CR>',   opts)
-map('n', '<Leader>4', '<Cmd>BufferLineGotoBuffer 4<CR>',   opts)
-map('n', '<Leader>5', '<Cmd>BufferLineGotoBuffer 5<CR>',   opts)
-map('n', '<Leader>6', '<Cmd>BufferLineGotoBuffer 6<CR>',   opts)
-map('n', '<Leader>7', '<Cmd>BufferLineGotoBuffer 7<CR>',   opts)
-map('n', '<Leader>8', '<Cmd>BufferLineGotoBuffer 8<CR>',   opts)
-map('n', '<Leader>9', '<Cmd>BufferLineGotoBuffer 9<CR>',   opts)
-map('n', '<C-CR>',    '<Cmd>BufferLineLast<CR>',     opts)
-map('n', '<A-p>',     '<Cmd>BufferLinePin<CR>',      opts)
-map('n', '<A-d>',     '<Cmd>BufferLineClose<CR>',    opts)
-map('n', '<C-s>',     '<Cmd>BufferLinePick<CR>', opts)
+map('n', '<Leader>h', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<Leader>l', '<Cmd>BufferNext<CR>',     opts)
+map('n', '<Leader>1', '<Cmd>BufferGotoBuffer 3<CR>',   opts)
+map('n', '<Leader>2', '<Cmd>BufferGotoBuffer 3<CR>',   opts)
+map('n', '<Leader>3', '<Cmd>BufferGotoBuffer 3<CR>',   opts)
+map('n', '<Leader>4', '<Cmd>BufferGotoBuffer 4<CR>',   opts)
+map('n', '<Leader>5', '<Cmd>BufferGotoBuffer 5<CR>',   opts)
+map('n', '<Leader>6', '<Cmd>BufferGotoBuffer 6<CR>',   opts)
+map('n', '<Leader>7', '<Cmd>BufferGotoBuffer 7<CR>',   opts)
+map('n', '<Leader>8', '<Cmd>BufferGotoBuffer 8<CR>',   opts)
+map('n', '<Leader>9', '<Cmd>BufferGotoBuffer 9<CR>',   opts)
+map('n', '<C-CR>',    '<Cmd>BufferLast<CR>',     opts)
+map('n', '<A-p>',     '<Cmd>BufferPin<CR>',      opts)
+map('n', '<A-d>',     '<Cmd>BufferClose<CR>',    opts)
+map('n', '<C-s>',     '<Cmd>BufferPick<CR>', opts)
 
 
 -- barbar.nvim
