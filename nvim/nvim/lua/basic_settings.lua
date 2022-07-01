22cd7f19e5ea88a42026dbbd9e8495bc51625d28
Ratheesh S <ratheeshreddy@gmail.com>
2022-07-01 10:31:42 +0530

    basic_settings: misc trivial changes

--- nvim/nvim/lua/basic_settings.lua
+++ nvim/nvim/lua/basic_settings.lua
@@ -1,42 +1,48 @@
+-- Copyright (c) 2022 Ratheesh
+-- License: MIT
+
 -- Basic ViM settings
+
 -- vim.opt.shadafile = "NONE"
-vim.opt.shada = "%,<300,'100,/10,:100,s100,h,f1,n$HOME/.config/nvim/shada.info"
-vim.opt.wrap = false
-vim.opt.list = true
-vim.opt.listchars = { tab = "  ", extends = "", precedes = "" }
-vim.opt.scrolloff = 5
--- vim.opt.whichwrap = "<,>,\[,\]"
-vim.opt.title = true
-vim.opt.clipboard = "unnamedplus"
-vim.opt.cmdheight = 0
-vim.opt.smartindent = true
-vim.opt.tabstop = 2
-vim.opt.signcolumn = "yes"
-vim.opt.shiftwidth = 2
+vim.opt.shada          = "%,<300,'100,/10,:100,s100,h,f1,n$HOME/.config/nvim/shada.info"
+vim.opt.wrap           = false
+vim.opt.list           = true
+vim.opt.listchars      = { tab = "  ", extends = "", precedes = "" }
+vim.opt.scrolloff      = 5
+-- vim.opt.whichwrap   = "<,>,\[,\]"
+vim.opt.title          = true
+vim.opt.clipboard      = "unnamedplus"
+vim.opt.cmdheight      = 0
+vim.opt.smartindent    = true
+vim.opt.tabstop        = 2
+vim.opt.signcolumn     = "yes"
+vim.opt.shiftwidth     = 2
 vim.opt.shortmess:append("sI")
-vim.opt.smartcase = true
-vim.opt.ignorecase = true
-vim.opt.number = true
+vim.opt.smartcase      = true
+vim.opt.ignorecase     = true
+vim.opt.number         = true
 vim.opt.relativenumber = true
-vim.opt.colorcolumn = "80"
-vim.opt.cursorline = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.termguicolors = true
-vim.opt.timeoutlen = 750
-vim.opt.undofile = true
-vim.opt.updatetime = 250
-vim.opt.shell = "/bin/zsh"
-vim.opt.lazyredraw = true
-vim.opt.mouse = "a"
-vim.opt.pumheight = 12
-vim.opt.completeopt = "menu,menuone,noselect"
-vim.opt.showmode = false
-vim.opt.confirm = true
-vim.opt.laststatus = 3
+vim.opt.colorcolumn    = "80"
+vim.opt.cursorline     = true
+vim.opt.splitbelow     = true
+vim.opt.splitright     = true
+vim.opt.termguicolors  = true
+vim.opt.timeoutlen     = 750
+vim.opt.undofile       = true
+vim.opt.updatetime     = 250
+vim.opt.shell          = "/bin/zsh"
+vim.opt.lazyredraw     = true
+vim.opt.mouse          = "a"
+vim.opt.pumheight      = 12
+vim.opt.completeopt    = "menu,menuone,noselect"
+vim.opt.showmode       = false
+vim.opt.confirm        = true
+vim.opt.laststatus     = 3
 
 vim.g.mapleader                = " "
 vim.g.maplocalleader           = ","
+
+-- Disable unwanted in-built plugins
 vim.g.loaded_2html_plugin      = 1
 vim.g.loaded_getscript         = 1
 vim.g.loaded_getscriptPlugin   = 1
@@ -57,12 +63,12 @@ vim.g.loaded_zipPlugin         = 1
 vim.g.do_filetype_lua          = 1
 vim.g.did_load_filetypes       = 0
 
-_G.colors = {
-	red = "#BF616A", teal = "#97B7D7", one_bg = "#373D49", lightbg = "#3B4252", blue = "#81A1C1",
-	cyan = "#5E81AC", black = "#2E3440", orange = "#D08770", one_bg2 = "#434C5E", foreground = "#E5E9F0",
-	grey = "#4B515D", green = "#A3BE8C", purple = "#8FBCBB", one_bg3 = "#4C566A", light_grey = "#646A76",
-	line = "#3A404C", white = "#D8DEE9", yellow = "#EBCB8B", lightbg2 = "#393F4B", dark_purple = "#B48EAD",
-	pink = "#D57780", black2 = "#343A46", grey_fg = "#606672", baby_pink = "#DE878F", darker_black = "#2A303C",
-}
+-- _G.colors = {
+-- 	red = "#BF616A", teal = "#97B7D7", one_bg = "#373D49", lightbg = "#3B4252", blue = "#81A1C1",
+-- 	cyan = "#5E81AC", black = "#2E3440", orange = "#D08770", one_bg2 = "#434C5E", foreground = "#E5E9F0",
+-- 	grey = "#4B515D", green = "#A3BE8C", purple = "#8FBCBB", one_bg3 = "#4C566A", light_grey = "#646A76",
+-- 	line = "#3A404C", white = "#D8DEE9", yellow = "#EBCB8B", lightbg2 = "#393F4B", dark_purple = "#B48EAD",
+-- 	pink = "#D57780", black2 = "#343A46", grey_fg = "#606672", baby_pink = "#DE878F", darker_black = "#2A303C",
+-- }
 
 -- End of File
