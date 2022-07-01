d16b1ff8e1de23689407ebfa74bb973064f8b257
Ratheesh S <ratheeshreddy@gmail.com>
2022-07-01 09:21:07 +0530

    Add autocmds module

---
+++ nvim/nvim/lua/autocmds.lua
@@ -0,0 +1,28 @@
+-- autocmds
+
+local group = vim.api.nvim_create_augroup("AutoCommands", { clear = true })
+vim.api.nvim_create_autocmd("Filetype", { pattern = "sh", group = group,
+	command = "setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4" })
+vim.api.nvim_create_autocmd("FocusLost,InsertLeave,CursorHold", { group = group,
+	command = "silent! update" })
+vim.api.nvim_create_autocmd("QuickFixCmdPost", { pattern = "[^l]*", group = group,
+	callback = function() TroubleQuickFixPost("quickfix") end })
+vim.api.nvim_create_autocmd("QuickFixCmdPost", { pattern = "l*", group = group,
+	callback = function() TroubleQuickFixPost("loclist") end })
+vim.api.nvim_create_autocmd("TextYankPost", { group = group,
+	callback = function() vim.highlight.on_yank({ higroup="IncSearch", timeout=300 }) end })
+
+
+local cursorGrp = vim.api.nvim_create_augroup("cursorGrp", { clear = true })
+vim.api.nvim_create_autocmd( "InsertLeave" , { group = cursorGrp, command = "hi CursorLineNr guifg=#eeeeee guibg=#b16286" })
+vim.api.nvim_create_autocmd( "InsertEnter" , { group = cursorGrp, command = "hi CursorLineNr guifg=#eeeeee guibg=#2AA198" })
+
+-- vim.api.nvim_create_autocmd(
+--   { "InsertLeave", "WinEnter" },
+--   { pattern = "*", command = "set relativenumber", group = cursorGrp })
+--
+-- vim.api.nvim_create_autocmd(
+--   { "InsertEnter", "WinLeave" },
+--   { pattern = "*", command = "set norelativenumber", group = cursorGrp })
+
+-- End of File
