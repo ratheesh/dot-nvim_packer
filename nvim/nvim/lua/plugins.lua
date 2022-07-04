9e79847a39d371a6483ba3eb7e9e2eb983a4ef9f
Ratheesh S <ratheeshreddy@gmail.com>
2022-07-04 16:48:46 +0530

    Add nvim-treesitter-textsubjects plugin

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -697,6 +697,22 @@ require("packer").startup({ function(use)
 		end
 	})
 	use({
+		"RRethy/nvim-treesitter-textsubjects",
+		config = function()
+			require('nvim-treesitter.configs').setup {
+				textsubjects = {
+					enable = true,
+					prev_selection = ',', -- (Optional) keymap to select the previous selection
+					keymaps = {
+						['.'] = 'textsubjects-smart',
+						[';'] = 'textsubjects-container-outer',
+						['i;'] = 'textsubjects-container-inner',
+					},
+				},
+			}
+		end
+	})
+	use({
 		"windwp/nvim-autopairs",
 		config = function()
 			require('nvim-autopairs').setup({
