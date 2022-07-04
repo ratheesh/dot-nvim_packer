14018097015dc0e3cf7d6574a7efad321c79dafd
Ratheesh S <ratheeshreddy@gmail.com>
2022-07-04 11:59:48 +0530

    gitsigns: Add user command to stage partial hunks

--- nvim/nvim/lua/keybindings.lua
+++ nvim/nvim/lua/keybindings.lua
@@ -94,4 +94,7 @@ map("s", "<C-h>" ,"<cmd>lua require('luasnip').jump(-1)<CR>")
 map("i", "<C-l>" ,"<cmd>lua require('luasnip').jump(1)<CR>")
 map("i", "<C-h>" ,"<cmd>lua require('luasnip').jump(-1)<CR>")
 
+-- gitsigns
+vim.api.nvim_create_user_command("Stage", function(t) require('gitsigns').stage_hunk({ t.line1, t.line2 }) end, { range = true })
+
 -- End of File
