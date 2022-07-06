9c0abbdecfbac4fd62aa7886c18b82b830a811b6
Ratheesh S <ratheeshreddy@gmail.com>
2022-07-06 13:47:17 +0530

    null-ls: Add assorted builtin sources

--- nvim/nvim/lua/plugins/null-ls.lua
+++ nvim/nvim/lua/plugins/null-ls.lua
@@ -5,15 +5,28 @@ function M.setup()
 	local ls = require("null-ls")
 	local nls_utils = require "null-ls.utils"
 	local sources = {
+		-- formatting
 		ls.builtins.formatting.shfmt,
 		ls.builtins.formatting.black,
 		ls.builtins.formatting.autopep8,
 		ls.builtins.formatting.isort,
 		-- ls.builtins.formatting.stylua,
 
+		-- diagnostics
 		ls.builtins.diagnostics.shellcheck,
+		ls.builtins.diagnostics.flake8,
+		ls.builtins.diagnostics.mypy,
+		ls.builtins.diagnostics.gitlint,
 		-- ls.builtins.diagnostics.vale.with({ args = '--config="$XDG_CONFIG_HOME/vale/vale.ini"' }),
 
+		-- completion
+		ls.builtins.completion.spell,
+
+		-- hover
+		ls.builtins.hover.dictionary,
+
+		-- code actions
+		ls.builtins.code_actions.refactoring,
 		ls.builtins.code_actions.gitsigns,
 		ls.builtins.code_actions.gitrebase,
 	}
