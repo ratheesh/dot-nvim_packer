1ace91345ab086ffc4986913fd60bfc63553f81d
Ratheesh S <ratheeshreddy@gmail.com>
2022-07-06 13:46:05 +0530

    lsp: add header + alignment

--- nvim/nvim/lua/plugins/lsp.lua
+++ nvim/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,16 @@
+-- Copyright (c) 2022 Ratheesh <ratheeshreddy@gmail.com>
+-- License: MIT
+-- LSP Configuration
 
 local M = {}
 
 function M.setup()
-	vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticError" })
-	vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticHint" })
-	vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticInfo" })
-	vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticWarn" })
+	vim.fn.sign_define("DiagnosticSignError" , { text = "" , texthl = "DiagnosticError" })
+	vim.fn.sign_define("DiagnosticSignHint"  , { text = "" , texthl = "DiagnosticHint"  })
+	vim.fn.sign_define("DiagnosticSignInfo"  , { text = "" , texthl = "DiagnosticInfo"  })
+	vim.fn.sign_define("DiagnosticSignWarn"  , { text = "" , texthl = "DiagnosticWarn"  })
 
-	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+	vim.lsp.handlers["textDocument/hover"]         = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
 	vim.lsp.buf.rename = {
@@ -24,18 +27,18 @@ function M.setup()
 			end
 
 			local win = require('plenary.popup').create(currName, {
-				title = "New Name",
-				style = "minimal",
-				borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-				relative = "cursor",
+				title           = "New Name",
+				style           = "minimal",
+				borderchars     = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+				relative        = "cursor",
 				borderhighlight = "FloatBorder",
-				titlehighlight = "Title",
-				highlight = tshl,
-				focusable = true,
-				width = 25,
-				height = 1,
-				line = "cursor+2",
-				col = "cursor-1"
+				titlehighlight  = "Title",
+				highlight       = tshl,
+				focusable       = true,
+				width           = 25,
+				height          = 1,
+				line            = "cursor+2",
+				col             = "cursor-1"
 			})
 
 			local map_opts = { noremap = true, silent = true }
@@ -68,30 +71,33 @@ function M.setup()
 			end
 		end
 	}
-	vim.diagnostic.config({ virtual_text = false, float = { show_header = false, border = "rounded" } })
+	vim.diagnostic.config({ virtual_text = false, float = { show_header = false, border = "rounded" }})
+
 	local function on_attach(client, bufnr)
 		local function bufmap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
 		local opts = { noremap = true, silent = true }
-		bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-		bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-		bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-		bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-		bufmap("n", "gk", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+		bufmap("n" , "gD"         , "<cmd>lua vim.lsp.buf.declaration()<CR>"       , opts)
+		bufmap("n" , "gd"         , "<cmd>lua vim.lsp.buf.definition()<CR>"        , opts)
+		bufmap("n" , "K"          , "<cmd>lua vim.lsp.buf.hover()<CR>"             , opts)
+		bufmap("n" , "gi"         , "<cmd>lua vim.lsp.buf.implementation()<CR>"    , opts)
+		bufmap("n" , "gk"         , "<cmd>lua vim.lsp.buf.signature_help()<CR>"    , opts)
+		bufmap("n" , "<leader>D"  , "<cmd>lua vim.lsp.buf.type_definition()<CR>"   , opts)
+		bufmap("n" , "<leader>rn" , "<cmd>lua vim.lsp.buf.rename.float()<CR>"      , opts)
+		bufmap("n" , "<leader>ca" , "<cmd>lua vim.lsp.buf.code_action()<CR>"       , opts)
+		bufmap("v" , "<leader>ca" , "<cmd>lua vim.lsp.buf.range_code_action()<CR>" , opts)
+		bufmap("n" , "gr"         , "<cmd>lua vim.lsp.buf.references()<CR>"        , opts)
+		bufmap("n" , "<leader>e"  , "<cmd>lua vim.diagnostic.open_float(0, { scope = 'line' })<CR>" , opts)
+		bufmap("n" , "gx"         , "<cmd>lua vim.diagnostic.goto_prev()<CR>"      , opts)
+		bufmap("n" , "gz"         , "<cmd>lua vim.diagnostic.goto_next()<CR>"      , opts)
+		bufmap("n" , "<leader>q"  , "<cmd>lua vim.diagnostic.setloclist()<CR>"     , opts)
+		bufmap("n" , "<leader>f"  , "<cmd>lua vim.lsp.buf.formatting()<CR>"        , opts)
 		-- bufmap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
 		-- bufmap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
 		-- bufmap("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-		bufmap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-		bufmap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename.float()<CR>", opts)
-		bufmap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-		bufmap("v", "<leader>ca", "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts)
-		bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-		bufmap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float(0, { scope = 'line' })<CR>", opts)
-		bufmap("n", "gx", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-		bufmap("n", "gz", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-		bufmap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-		bufmap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
 		require("illuminate").on_attach(client)
+		-- require("virtualtypes").on_attach(client)
 	end
 
 	local lspconfig = require("lspconfig")
@@ -144,7 +150,6 @@ function M.setup()
 	})
 
 	-- Python
-	lspconfig.pyright.setup({ on_attach = on_attach, capabilities = capabilities })
 	lspconfig.jedi_language_server.setup({ on_attach = on_attach, capabilities = capabilities })
 	lspconfig.pyright.setup({
 		on_attach = on_attach,
