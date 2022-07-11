24c5ce80bed5c3f19492609efbc1ea6b5afd4a47
Ratheesh S <ratheeshreddy@gmail.com>
2022-07-11 10:33:08 +0530

    mini: disable jumps functionality

--- nvim/nvim/lua/plugins/mini.lua
+++ nvim/nvim/lua/plugins/mini.lua
@@ -8,22 +8,22 @@ function M.setup()
 	require("mini.indentscope").setup({
 		symbol = '╎',
 	})
-	require("mini.jump").setup({
-		{
-			-- Module mappings. Use `''` (empty string) to disable one.
-			mappings = {
-				forward       = 'f',
-				backward      = 'F',
-				forward_till  = 't',
-				backward_till = 'T',
-				repeat_jump   = ';',
-			},
-			delay = {
-				highlight = 250,
-				idle_stop = 10000000,
-			},
-		}
-	})
+	-- require("mini.jump").setup({
+	-- 	{
+	-- 		-- Module mappings. Use `''` (empty string) to disable one.
+	-- 		mappings = {
+	-- 			forward       = 'f',
+	-- 			backward      = 'F',
+	-- 			forward_till  = 't',
+	-- 			backward_till = 'T',
+	-- 			repeat_jump   = ';',
+	-- 		},
+	-- 		delay = {
+	-- 			highlight = 250,
+	-- 			idle_stop = 10000000,
+	-- 		},
+	-- 	}
+	-- })
 	-- require('mini.surround').setup({
 	-- 	custom_surroundings = {
 	-- 		['('] = { output = { left = '( ', right = ' )' } },
