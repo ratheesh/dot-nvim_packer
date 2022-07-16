2b5e6f4ee703143db417b4c87463a76695d3fb63
Ratheesh S <ratheeshreddy@gmail.com>
2022-07-16 08:58:04 +0530

    gitsigns: map hunk textobject

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -57,7 +57,20 @@ require("packer").startup({ function(use)
 	use("nvim-lua/plenary.nvim")
 	use({
 		"lewis6991/gitsigns.nvim",
-		config = function() require("gitsigns").setup() end
+		config = function()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					-- local gs = package.loaded.gitsigns
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+				end
+			})
+		end
 	})
 	use({
 		"nvim-treesitter/nvim-treesitter",
