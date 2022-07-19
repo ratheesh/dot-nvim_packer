ebcb23643139e0b10854596b31d8d31a6b124ae8
Ratheesh S <ratheeshreddy@gmail.com>
2022-07-19 11:59:26 +0530

    hydra:git: disable word diff mode upon exit
    Also, move the hint win to middle-right.

--- nvim/nvim/lua/plugins/hydra.lua
+++ nvim/nvim/lua/plugins/hydra.lua
@@ -5,11 +5,14 @@
 local Hydra = require('hydra')
 local gitsigns = require('gitsigns')
 local hint = [[
-	_j_: next hunk   _s_: stage hunk        _d_: show deleted   _b_: blame line				^ ^
-	_k_: prev hunk   _u_: undo stage hunk   _p_: preview hunk   _B_: blame show full  ^ ^
-	_R_: reset hunk  _S_: stage buffer      _/_: show base file   ^ ^
+ _j_: next hunk       _k_: prev hunk       ^ ^
+ _d_: show deleted    _D_: Word diff       ^ ^
+ _u_: undo stage hunk _R_: reset hunk      ^ ^
+ _b_: blame line      _B_: blame show full ^ ^
+ _s_: stage hunk      _S_: stage buffer    ^ ^
+ _/_: show base file  _p_: preview hunk    ^ ^
 ^
-^ ^              _<Enter>_: Neogit      _q_/<Esc>: exit
+^_<Enter>_: Neogit    q_/<Esc>: exit
 ]]
 Hydra({
 	name = "Git",
@@ -18,7 +21,7 @@ Hydra({
 		color = 'pink',
 		invoke_on_body = true,
 		hint = {
-			position = 'bottom',
+			position = 'middle-right',
 			border   = 'rounded'
 		},
 		on_enter = function()
@@ -30,6 +33,7 @@ Hydra({
 			gitsigns.toggle_signs(true)
 			gitsigns.toggle_linehl(false)
 			gitsigns.toggle_deleted(false)
+			gitsigns.toggle_word_diff(false)
 			vim.cmd 'echo' -- clear the echo area
 		end
 	},
@@ -46,18 +50,19 @@ Hydra({
 			vim.schedule(function() gitsigns.prev_hunk() end)
 			return '<Ignore>'
 		end, { expr = true } },
-		{ 's',       '<cmd>Gitsigns stage_hunk<CR>', { silent = true } },
-		{ 'u',       gitsigns.undo_stage_hunk, { desc = 'undo staged hunk' }},
-		{ 'S',       gitsigns.stage_buffer, { desc = 'stage buffer' }},
-		{ 'R',       gitsigns.reset_hunk      },
-		{ 'p',       gitsigns.preview_hunk    },
-		{ 'd',       gitsigns.toggle_deleted, { nowait = true } },
-		{ 'b',       gitsigns.blame_line },
-		{ 'B',       function() gitsigns.blame_line { full = true } end },
-		{ '/',       gitsigns.show, { exit = true }     },
+		{ 's', '<cmd>Gitsigns stage_hunk<CR>', { silent = true } },
+		{ 'u', gitsigns.undo_stage_hunk, { desc = 'undo staged hunk' } },
+		{ 'S', gitsigns.stage_buffer, { desc = 'stage buffer' } },
+		{ 'R', gitsigns.reset_hunk },
+		{ 'p', gitsigns.preview_hunk },
+		{ 'd', gitsigns.toggle_deleted, { nowait = true } },
+		{ 'D', gitsigns.toggle_word_diff, { nowait = true } },
+		{ 'b', gitsigns.blame_line },
+		{ 'B', function() gitsigns.blame_line { full = true } end },
+		{ '/', gitsigns.show, { exit = true } },
 		{ '<Enter>', '<cmd>Neogit<CR>', { exit = true } },
-		{ '<Esc>',   nil, { exit = true, nowait = true }},
-		{ 'q',       nil, { exit = true, nowait = true }},
+		{ '<Esc>', nil, { exit = true, nowait = true } },
+		{ 'q', nil, { exit = true, nowait = true } },
 	}
 })
 
