eae03a67c9a945c6b646c795d2fc3c39229d7bca
Ratheesh S <ratheeshreddy@gmail.com>
2022-08-15 21:04:41 +0530

    refactor: alpha: assorted config for startify theme
    * Custom configuration for the startify theme
    * q -> buffer close instead of exiting app
    * X -> Exit NeoViM

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -786,7 +786,35 @@ require("packer").startup({ function(use)
 		event    = "VimEnter",
 		requires = { 'kyazdani42/nvim-web-devicons' },
 		config   = function ()
-			require'alpha'.setup(require'alpha.themes.startify'.config)
+			-- require'alpha'.setup(require'alpha.themes.startify'.config)
+			local alpha    = require'alpha'
+			local startify = require'alpha.themes.startify'
+			startify.section.header.val = {
+				[[                                   __                ]],
+				[[      ___     ___    ___   __  __ /\_\    ___ ___    ]],
+				[[     / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+				[[    /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+				[[    \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+				[[     \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+			}
+			startify.section.top_buttons.val = {
+				startify.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
+			}
+			startify.nvim_web_devicons.enabled = true
+			startify.section.bottom_buttons.val = {
+				startify.button( "q", "  Quit this Screen" , "<cmd>BufferClose<CR>"),
+				startify.button( "X", "  Quit NV!M" , "<cmd>qall<CR>"),
+			}
+			startify.section.footer = {
+				{ type = "text", val = "footer" },
+			}
+			-- ignore filetypes in MRU
+
+			startify.mru_opts.ignore = function(path, _)
+				return
+				(string.find(path, "COMMIT_EDITMSG"))
+			end
+			alpha.setup(startify.config)
 		end
 	})
 	-- The missing auto-completion for cmdline!
