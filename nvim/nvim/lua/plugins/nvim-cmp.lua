fd4cea48a17fa9683d7b560f2d3207ebff4c4c21
Ratheesh S <ratheeshreddy@gmail.com>
2022-08-18 16:41:22 +0530

    nvim-cmp: assorted changes

--- nvim/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/nvim/lua/plugins/nvim-cmp.lua
@@ -39,7 +39,7 @@ function M.setup()
 			completion    = cmp.config.window.bordered({
 				winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None"
 			}),
-			documentation = cmp.config.window.bordered({ winhighlight = "" }),
+			documentation = cmp.config.window.bordered({ winhighlight = "Normal:CmpDocNormal" }),
 		},
 		sources = {
 			-- { name = 'nvim_lsp_signature_help' },
@@ -62,27 +62,17 @@ function M.setup()
 		snippet = {
 			expand = function(args) require("luasnip").lsp_expand(args.body) end,
 		},
-		-- sorting = {
-		-- 	comparators = {
-		-- 		-- function(entry1, entry2)
-		-- 			--   local score1 = entry1.completion_item.score
-		-- 			--   local score2 = entry2.completion_item.score
-		-- 			--   if score1 and score2 then
-		-- 			--     return (score1 - score2) < 0
-		-- 			--   end
-		-- 			-- end,
-  --
-		-- 			-- The built-in comparators:
-		-- 			cmp.config.compare.offset,
-		-- 			cmp.config.compare.exact,
-		-- 			cmp.config.compare.score,
-		-- 			require('cmp-under-comparator').under,
-		-- 			cmp.config.compare.kind,
-		-- 			cmp.config.compare.sort_text,
-		-- 			cmp.config.compare.length,
-		-- 			cmp.config.compare.order,
-		-- 		},
-		-- 	},
+		sorting = {
+			comparators = {
+				cmp.config.compare.offset,
+				cmp.config.compare.exact,
+				cmp.config.compare.recently_used,
+				cmp.config.compare.kind,
+				cmp.config.compare.sort_text,
+				cmp.config.compare.length,
+				cmp.config.compare.order,
+			},
+		},
 		formatting = {
 			format = require('lspkind').cmp_format({
 				preset     = 'codicons',
