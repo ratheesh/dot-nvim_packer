f14a0b76c56ca2a009c2e176a800317ba7bc455a
Ratheesh S <ratheeshreddy@gmail.com>
2022-08-29 19:03:23 +0530

    refactor(null-ls): convert double quotes to single

--- nvim/nvim/lua/plugins/null-ls.lua
+++ nvim/nvim/lua/plugins/null-ls.lua
@@ -7,12 +7,12 @@ local M = {}
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 function M.setup()
-	local ls = require("null-ls")
-	local nls_utils = require "null-ls.utils"
+	local ls = require('null-ls')
+	local nls_utils = require('null-ls.utils')
 
 	local with_diagnostics_code = function(builtin)
 		return builtin.with {
-			diagnostics_format = "#{m} [#{c}]",
+			diagnostics_format = '#{m} [#{c}]',
 		}
 	end
 
@@ -31,7 +31,7 @@ function M.setup()
 		ls.builtins.formatting.autopep8,
 		ls.builtins.formatting.isort,
 		ls.builtins.formatting.prettier.with({
-			filetypes = { "html", "css", "yaml", "markdown", "json" },
+			filetypes = { 'html', 'css', 'yaml', 'markdown', 'json' },
 		}),
 		-- ls.builtins.formatting.stylua,
 
@@ -62,7 +62,7 @@ function M.setup()
 		save_after_format = false,
 		sources           = sources,
 		root_dir          = nls_utils.root_pattern ".git",
-		-- filetypes         = { "python", "lua", 'gitcommit', "c", "cpp", "html",  },
+		-- filetypes         = { 'python', 'lua', 'gitcommit', 'c', 'cpp', 'html',  },
 
 		on_init = function(new_client, _)
 			new_client.offset_encoding = 'utf-32'
@@ -75,7 +75,7 @@ function M.setup()
 					group = augroup,
 					buffer = bufnr,
 					callback = function()
-						if vim.bo.filetype == "python" then
+						if vim.bo.filetype == 'python' or vim.bo.filetype == 'html' then
 							vim.lsp.buf.format({bufnr = bufnr})
 						end
 					end,
