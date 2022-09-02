f0f66039258230baac0122fb587a9303b3d74976
Ratheesh S <ratheeshreddy@gmail.com>
2022-09-02 07:46:37 +0530

    feat(autopairs): Add rules for inserting smart space

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -814,6 +814,35 @@ require("packer").startup({ function(use)
 				map_cr                    = false
 			})
 			require("nvim-autopairs").add_rules(require "nvim-autopairs.rules.endwise-lua")
+
+			local npairs = require'nvim-autopairs'
+			local Rule   = require'nvim-autopairs.rule'
+
+			npairs.add_rules {
+				Rule(' ', ' ')
+				:with_pair(function (opts)
+					local pair = opts.line:sub(opts.col - 1, opts.col)
+					return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+				end),
+				Rule('( ', ' )')
+				:with_pair(function() return false end)
+				:with_move(function(opts)
+					return opts.prev_char:match('.%)') ~= nil
+				end)
+				:use_key(')'),
+				Rule('{ ', ' }')
+				:with_pair(function() return false end)
+				:with_move(function(opts)
+					return opts.prev_char:match('.%}') ~= nil
+				end)
+				:use_key('}'),
+				Rule('[ ', ' ]')
+				:with_pair(function() return false end)
+				:with_move(function(opts)
+					return opts.prev_char:match('.%]') ~= nil
+				end)
+				:use_key(']')
+			}
 		end
 	})
 	use ({
