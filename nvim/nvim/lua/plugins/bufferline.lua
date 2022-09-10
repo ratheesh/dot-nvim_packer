9d01aee7aba1e8fcc89067a76c138e34b104c2f1
Ratheesh S <ratheeshreddy@gmail.com>
2022-09-10 08:00:48 +0530

    feat(bufferline): enable hover functionality

--- nvim/nvim/lua/plugins/bufferline.lua
+++ nvim/nvim/lua/plugins/bufferline.lua
@@ -17,7 +17,12 @@ bufferline.setup({
 		diagnostics_indicator = function(count, level)
 			local icon = level:match("error") and " " or ""
 			return " " .. icon .. count
-		end
+		end,
+		hover = {
+			enabled = true,
+			delay   = 100,
+			reveal  = { 'close' }
+		},
 	},
 	highlights = {
 		fill = {
