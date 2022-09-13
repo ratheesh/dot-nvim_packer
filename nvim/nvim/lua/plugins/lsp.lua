d621b8d5656c22e302b09bf236301d1cd433fb7f
Ratheesh S <ratheeshreddy@gmail.com>
2022-09-13 12:16:21 +0530

    feat(lsp): Add `emmet_ls` LSP server

--- nvim/nvim/lua/plugins/lsp.lua
+++ nvim/nvim/lua/plugins/lsp.lua
@@ -333,6 +333,21 @@ function M.setup()
 		},
 	}
 
+	lspconfig.emmet_ls.setup({
+		on_init = on_init,
+		on_attach = on_attach,
+		capabilities = capabilities,
+		filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+		init_options = {
+			html = {
+				options = {
+					-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+					["bem.enabled"] = true,
+				},
+			},
+		}
+	})
+
 	-- Lua
 	lspconfig.sumneko_lua.setup(require("lua-dev").setup({
 		lspconfig = {
