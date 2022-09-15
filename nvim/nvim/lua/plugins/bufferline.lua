a9bc8e3da52e2d7ca7f8c567ba6392e43b7c7fcf
Ratheesh S <ratheeshreddy@gmail.com>
2022-09-15 17:52:05 +0530

    refactor(bufferline): change the render style + " -> '

--- nvim/nvim/lua/plugins/bufferline.lua
+++ nvim/nvim/lua/plugins/bufferline.lua
@@ -7,16 +7,18 @@ local bufferline = require('bufferline')
 bufferline.setup({
 	options = {
 		mode      = 'buffers',
-		numbers   = "ordinal",
+		numbers   = 'ordinal',
 		indicator = {
-			style   = 'none'
+			-- icon = '▌',
+			icon = ' ',
+			style   = 'underline'
 		},
-		separator_style = 'slant',
+		separator_style = 'none',
 		buffer_close_icon = '',
 		-- diagnostics       = 'nvim_lsp',
 		diagnostics_indicator = function(count, level)
-			local icon = level:match("error") and " " or ""
-			return " " .. icon .. count
+			local icon = level:match('error') and ' ' or ''
+			return ' ' .. icon .. count
 		end,
 		hover = {
 			enabled = true,
@@ -68,6 +70,12 @@ bufferline.setup({
 		separator = {
 			fg = '#1E2430',
 			bg = '#383a4c'
+		},
+		indicator_selected = {
+			fg = '#404a5c',
+			bg = '#404a5c',
+			sp = '#FF80A0',
+			bold = true,
 		},
 		tab = {
 			fg = '#988C99',
@@ -156,7 +164,7 @@ bufferline.setup({
 		fg = '<colour-value-here>',
 		bg = '<colour-value-here>',
 		sp = '<colour-value-here>'
-		bold = true,
+		bowld = true,
 		italic = true,
 		},
 		info = {
@@ -287,29 +295,29 @@ bufferline.setup({
 
 local function map(mode, lhs, rhs, opts)
 	local options = { noremap = true, silent = true }
-	if opts then options = vim.tbl_extend("force", options, opts) end
+	if opts then options = vim.tbl_extend('force', options, opts) end
 	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- Move to previous/next
-map('n', '<Leader>h', '<Cmd>BufferLineCyclePrev<CR>',    { desc = "Goto Previous Buffer" })
-map('n', '<Leader>l', '<Cmd>BufferLineCycleNext<CR>',    { desc = "Goto Next Buffer" 		})
+map('n', '<Leader>h', '<Cmd>BufferLineCyclePrev<CR>',    { desc = 'Goto Previous Buffer' })
+map('n', '<Leader>l', '<Cmd>BufferLineCycleNext<CR>',    { desc = 'Goto Next Buffer' 		})
 
 -- jump to buffer index
-map('n', '<Leader>1', '<Cmd>BufferLineGoToBuffer 1<CR>', { desc = "Goto Buffer 1"        })
-map('n', '<Leader>2', '<Cmd>BufferLineGoToBuffer 2<CR>', { desc = "Goto Buffer 2"        })
-map('n', '<Leader>3', '<Cmd>BufferLineGoToBuffer 3<CR>', { desc = "Goto Buffer 3"        })
-map('n', '<Leader>4', '<Cmd>BufferLineGoToBuffer 4<CR>', { desc = "Goto Buffer 4"        })
-map('n', '<Leader>5', '<Cmd>BufferLineGoToBuffer 5<CR>', { desc = "Goto Buffer 5"        })
-map('n', '<Leader>6', '<Cmd>BufferLineGoToBuffer 6<CR>', { desc = "Goto Buffer 6"        })
-map('n', '<Leader>7', '<Cmd>BufferLineGoToBuffer 7<CR>', { desc = "Goto Buffer 7"        })
-map('n', '<Leader>8', '<Cmd>BufferLineGoToBuffer 8<CR>', { desc = "Goto Buffer 8"        })
-map('n', '<Leader>9', '<Cmd>BufferLineGoToBuffer 9<CR>', { desc = "Goto Buffer 9"        })
+map('n', '<Leader>1', '<Cmd>BufferLineGoToBuffer 1<CR>', { desc = 'Goto Buffer 1'        })
+map('n', '<Leader>2', '<Cmd>BufferLineGoToBuffer 2<CR>', { desc = 'Goto Buffer 2'        })
+map('n', '<Leader>3', '<Cmd>BufferLineGoToBuffer 3<CR>', { desc = 'Goto Buffer 3'        })
+map('n', '<Leader>4', '<Cmd>BufferLineGoToBuffer 4<CR>', { desc = 'Goto Buffer 4'        })
+map('n', '<Leader>5', '<Cmd>BufferLineGoToBuffer 5<CR>', { desc = 'Goto Buffer 5'        })
+map('n', '<Leader>6', '<Cmd>BufferLineGoToBuffer 6<CR>', { desc = 'Goto Buffer 6'        })
+map('n', '<Leader>7', '<Cmd>BufferLineGoToBuffer 7<CR>', { desc = 'Goto Buffer 7'        })
+map('n', '<Leader>8', '<Cmd>BufferLineGoToBuffer 8<CR>', { desc = 'Goto Buffer 8'        })
+map('n', '<Leader>9', '<Cmd>BufferLineGoToBuffer 9<CR>', { desc = 'Goto Buffer 9'        })
 
-map('n', '<A-p>',    '<Cmd>BufferLineTogglePin<CR>',    { desc = "Pin Buffer"           })
-map("n", '<A-d>',    '<Cmd>bdelete<CR>',                { desc = 'Delete Current Buf'   })
-map('n', '<C-s>',    '<Cmd>BufferLinePick<CR>',         { desc = "Pick Buffer"          })
-map('n', '<A->>',    '<Cmd>BufferLineMoveNext<CR>',     { desc = "Move Buffer Right"    })
-map('n', '<A-<>',    '<Cmd>BufferLineMovePrev<CR>',     { desc = "Move Buffer Left"     })
+map('n', '<A-p>',    '<Cmd>BufferLineTogglePin<CR>',    { desc = 'Pin Buffer'           })
+map('n', '<A-d>',    '<Cmd>bdelete<CR>',                { desc = 'Delete Current Buf'   })
+map('n', '<C-s>',    '<Cmd>BufferLinePick<CR>',         { desc = 'Pick Buffer'          })
+map('n', '<A->>',    '<Cmd>BufferLineMoveNext<CR>',     { desc = 'Move Buffer Right'    })
+map('n', '<A-<>',    '<Cmd>BufferLineMovePrev<CR>',     { desc = 'Move Buffer Left'     })
 
 -- End of File
