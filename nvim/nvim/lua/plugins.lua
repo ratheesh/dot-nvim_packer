51c100774a0848b849bc6c19910af3eb19ce29a0
Ratheesh S <ratheeshreddy@gmail.com>
2022-11-16 18:05:55 +0530

    fix(lsp-inlayhints): fix for Comment.nvim

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -600,7 +600,12 @@ require('packer').startup({ function(use)
 					extra    = true,
 					extended = false,
 				},
-				pre_hook  = nil,
+				pre_hook = function(ctx)
+					local line_start = (ctx.srow or ctx.range.srow) - 1
+					local line_end = ctx.erow or ctx.range.erow
+					-- require("lsp-inlayhints.core").clear(0, line_start, line_end)
+					vim.api.nvim_buf_clear_namespace(0, -1, line_start, line_end)
+				end,
 				post_hook = nil,
 			})
 		end
